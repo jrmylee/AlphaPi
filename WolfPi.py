556a0bc1e1d97dd829e37d4804e1eb3295aabf27
Jeremy Lee <jrmylee@gmail.com>
2016-06-24 00:16:40 -0700

    GUI Progress #1

--- WolfPi.py
+++ WolfPi.py
@@ -1,31 +1,47 @@
 import wolframalpha
 from tkinter import *
+from PIL import ImageTk, Image
+import urllib.request
+
+
+
+
+client = wolframalpha.Client('X969A9-QP6K293UUR')
+root = Tk()
+txtBox = Text(root)
+entry = Entry(root)
+
+
 class WolframAlpha():
-    
 
-    def showPods(self):
-        x = input('What is your question?')
-        client = wolframalpha.Client('X969A9-QP6K293UUR')
-        x = self.translate(x)
-        res = client.query(x)
+    def main(self):
+        button = Button(root,text = "Get Rekt", command=self.transmit)
+        labe = Label(root,text="Search", bg = "black", fg = "white")
 
-        for pod in res.pods:
-            if pod.text:
-                print(pod.text)
-            elif pod.img:
-                print(pod.img)
-    def makeWindow(image):
-        root = Tk()
+        txtBox.grid(row=0)
+        labe.grid(row = 1)
+        entry.grid(row=1, column=1)
+        button.grid(row=1, column=2)
+        root.mainloop()
 
-        topFrame = Frame(root)
-        topFrame.pack()
 
-        bottomFrame = Frame(root)
-        bottomFrame.pack(side=BOTTOM)
+    def transmit(self):
+        res = client.query(entry.get())
+        if len(res.pods) > 0:
+            for pod in res.pods:
+                if pod.text:
+                    txtBox.insert('1.0', pod.text + '\n')
+                elif pod.img:
+                    im = Image.open(urllib.request.urlopen(pod.img))
+
+                    photo = PhotoImage(im)
+                    label = Label(root, image = photo)
+                    label.grid(row = 0, column = 1)
     def translate(self, String):
         if String[0:2] == "d:":
-            return "derivative of"
+            return "ddx" + String[2:len(String)]
         else:
             return String
+
 app = WolframAlpha()
-app.showPods()
+app.main()
